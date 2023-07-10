03ba950f5c255b9614b8a8e186742278fa111249
Alexandra <alexandra@MacBook-Air-Alexandra.local>
2023-07-10 19:42:57 +0400

    refactor: add white-space in jest.config

--- RSS-CSS-Selectors/jest.config.js
+++ RSS-CSS-Selectors/jest.config.js
@@ -1,10 +1,12 @@
 module.exports =
-{roots: ['<rootDir>/test'],
-testMatch: [
-  '/__tests__//*.+(ts|tsx|js)',
-  '**/?(*.)+(spec|test).+(ts|tsx|js)',
-],
-transform: {
-  '^.+\\.(ts|tsx)$': 'ts-jest',
-},
-testEnvironment: 'jsdom',}
+{
+  roots: ['<rootDir>/test'],
+  testMatch: [
+    '/__tests__//*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testEnvironment: 'jsdom',
+}
